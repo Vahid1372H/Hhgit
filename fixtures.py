6d1bd8dc20ce998f58c631e895fcf20a8629e167
Vahid Hajikhani <vahid_hajikhani@yahoo.com>
2025-11-26 13:29:43 +0330

    fixtures.py is created

---
+++ fixtures.py
@@ -0,0 +1,7 @@
+library = [
+    {"title": "ai", "author": "Vahid", "year": 2020},
+    {"title": "quantum", "author": "Ali", "year": 2021},
+    {"title": "6G", "author": "Reza", "year": 2022},
+]
+
+
