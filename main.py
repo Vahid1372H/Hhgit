4072fb8484eb30b5c9e5d415b242af6a6e21488e
Vahid Hajikhani <vahid_hajikhani@yahoo.com>
2025-11-26 19:27:45 +0330

    main.py is created

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fixtures import library
 
 def add_book(title: str, author:str, year: str) -> list:
+    """_summary_
+
+    Args:
+        title (str): _description_
+        author (str): _description_
+        year (str): _description_
+
+    Returns:
+        list: _description_
+    """
     try:
         if isinstance(library or title or author or year, list):
             library.append({"title":title, "author": author, "year":year})
