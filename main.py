7042551ae6f789a3d34136485a3bd9b4966934ad
Vahid Hajikhani <vahid_hajikhani@yahoo.com>
2025-11-26 19:20:28 +0330

    main.py is created

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fixtures import library
 
-
-def add_book(library, title, author, year):
-    library.append({"title": title, "author": author, "year": year})
-print(add_book((library, "Deep Learning", "Goodfellow", 2016)
-))
+def add_book(title: str, author:str, year: str) -> list:
+    try:
+        if isinstance(library or title or author or year, list):
+            library.append({"title":title, "author": author, "year":year})
+            return library
+        else:
+            return "Error..."
+    except ValueError as v:
+        return f"The value is incorrect: str{(v)}"
+print(add_book("qc", "ahmad", "2025"))
