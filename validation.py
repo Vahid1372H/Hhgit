fc84fc8ec7d870f3c466d8b0bd40c492622c40d5
Vahid Hajikhani <vahid_hajikhani@yahoo.com>
2025-11-26 19:26:40 +0330

    validation.py is created

--- validation.py
+++ validation.py
@@ -1,4 +1,12 @@
 def is_list_library(list_library: list) -> bool:
+    """_summary_
+
+    Args:
+        list_library (list): _description_
+
+    Returns:
+        bool: _description_
+    """
     if not isinstance(list_library, list):
         return False
     return True
